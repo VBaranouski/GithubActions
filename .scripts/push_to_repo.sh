c40fde468bd32a2085a5674f2073cb5830bdd9ad
Vlad Baranouski <vladislav.baranovski@gmail.com>
2025-02-13 14:41:08 +0200

    Update push_to_repo.sh

--- .scripts/push_to_repo.sh
+++ .scripts/push_to_repo.sh
@@ -6,4 +6,4 @@ git config --global user.email "$GIT_USER_EMAIL"
 git config --global user.name "$GIT_USER"
 git add -A
 git commit -m "Update " || echo "No changes to commit"
-git push
+git push git@github.com:${{ github.repository }}.git main
