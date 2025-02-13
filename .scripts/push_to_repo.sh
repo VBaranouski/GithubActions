3b3fbc07813f62373f748cfaca2504be8110abad
Vlad Baranouski <vladislav.baranovski@gmail.com>
2025-02-13 15:24:27 +0200

    Update push_to_repo.sh

--- .scripts/push_to_repo.sh
+++ .scripts/push_to_repo.sh
@@ -4,7 +4,8 @@
 cd "$REPO_DIR"
 git config --global user.email "$GIT_USER_EMAIL"
 git config --global user.name "$GIT_USER"
-git remote set-url origin https://x-access-token:"$ACCESS_TOKEN"@github.com/"$GIT_NAME".git
+echo "$GIT_USER" "$GIT_USER_EMAIL"
+git remote set-url origin https://x-access-token:"$GITHUB_TOKEN"@github.com/"$GIT_NAME".git
 git add -A
 git commit -m "Update " || echo "No changes to commit"
 git push origin main
