66666e150cb196222d0da998a8269e4c05b2c7b6
Vlad Baranouski <vladislav.baranovski@gmail.com>
2025-02-13 15:49:21 +0200

    Update push_to_repo.sh

--- .scripts/push_to_repo.sh
+++ .scripts/push_to_repo.sh
@@ -9,6 +9,7 @@ git config --global user.name "$GIT_USER"
 
 # Setup remote repository
 git remote set-url origin "https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+git remote -v
 
 # Commit changes
 git add -A
