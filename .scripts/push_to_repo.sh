76b330299fdbd6b1d07c1218e8b4f3c5e10506cb
Vlad Baranouski <vladislav.baranovski@gmail.com>
2025-02-13 14:30:24 +0200

    Update push_to_repo.sh

--- .scripts/push_to_repo.sh
+++ .scripts/push_to_repo.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-WORK_DIR="/tmp/Repo"
-REPO_DIR="$WORK_DIR/"
-$GIT_USER_EMAIL="mail@mail.tr"
-$GIT_USER="vbars"
-
 # Добавляем изменения и отправляем в репозиторий
-cd "$REPO_DIR"
-git config --global user.email "$GIT_USER_EMAIL"
-git config --global user.name "$GIT_USER"
+cd "${{ env.REPO_DIR }}"
+git config --global user.email "${{ env.GIT_USER_EMAIL }}"
+git config --global user.name "${{ env.GIT_USER }}"
 git add -A
 git commit -m "Update " || echo "No changes to commit"
 git push
